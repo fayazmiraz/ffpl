6de8dc46f78199065a14cbea2554728bcbffe9e9
Fayaz Ahmed <fayazmiraz@users.noreply.github.com>
2023-04-09 16:54:21 +0600

    Rename to dataDirectory for consistency

--- modules/firefox-profiles.mjs
+++ modules/firefox-profiles.mjs
@@ -1,11 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import ini from 'ini';
-import firefoxUserDirectory from './firefox-data-directory.mjs';
+import firefoxDataDirectory from './firefox-data-directory.mjs';
 
 export default function (platform) {
-  const userDirectory = firefoxUserDirectory(platform);
-  const data = fs.readFileSync(path.join(userDirectory, 'profiles.ini'), { encoding: 'utf8' });
+  const dataDirectory = firefoxDataDirectory(platform);
+  const data = fs.readFileSync(path.join(dataDirectory, 'profiles.ini'), { encoding: 'utf8' });
   const parsedData = ini.parse(data);
 
   return Object.entries(parsedData).filter(([key, value]) => {
